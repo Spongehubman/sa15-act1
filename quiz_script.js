2cdab43ec79bd987097e5a5a4c5a049096e4cb4e
Nicholas Folis <heffawoozle77@gmail.com>
2024-04-13 13:46:58 -0500

    In 'quiz.html', all remaining 'submit' buttons were given a unique 'id' specifier. In 'quiz_script.js', new event listeners were created for four more events that deal with checking whether questions 2-5 were correct or not; this was done in the same way described for question 1.

--- quiz_script.js
+++ quiz_script.js
@@ -4,12 +4,60 @@
 //question1.addEventListener.preventDefault()
 
 document.getElementById("submit1").addEventListener("click", question1)
+document.getElementById("submit2").addEventListener("click", question2)
+document.getElementById("submit3").addEventListener("click", question3)
+document.getElementById("submit4").addEventListener("click", question4)
+document.getElementById("submit5").addEventListener("click", question5)
 
 function question1() {
     event.preventDefault()
     var real_answer = document.getElementById("html")
     //var c2 = document.getElementById("ruby")
     //c2.checked = true
+
+    if (real_answer.checked == true) {
+        console.log("correct")
+    }   else    {
+        console.log("incorrect")
+    }
+}
+
+function question2() {
+    event.preventDefault()
+    var real_answer = document.getElementById("javascript")
+
+    if (real_answer.checked == true) {
+        console.log("correct")
+    }   else    {
+        console.log("incorrect")
+    }
+}
+
+function question3() {
+    event.preventDefault()
+    var real_answer = document.getElementById("failtest")
+
+    if (real_answer.checked == true) {
+        console.log("correct")
+    }   else    {
+        console.log("incorrect")
+    }
+}
+
+function question4() {
+    event.preventDefault()
+    var real_answer = document.getElementById("minimum2")
+
+    if (real_answer.checked == true) {
+        console.log("correct")
+    }   else    {
+        console.log("incorrect")
+    }
+}
+
+function question5() {
+    event.preventDefault()
+    var real_answer = document.getElementById("jaguar")
 
     if (real_answer.checked == true) {
         console.log("correct")
