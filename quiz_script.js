b8bb9705fdc0b55f36ec427895ed6169f28fa517
Nicholas Folis <heffawoozle77@gmail.com>
2024-04-13 13:32:16 -0500

    In 'quiz.script.js', I added an event listener to allow a response to be logged to the console based on what answer was selected for question 1. In 'quiz.html', I have added an 'h1' header to the top of the page describing what the quiz is about, and I have added an 'id' specifier inside of the first 'Submit Answer' button to allow the new 'js' function to work. Furthermore, each 'input' button in the 'question_7' section received an 'id' and a 'value' specifier, and the remaining 'label' tags received a corresponding 'for' specifier for each question in that section.

--- quiz_script.js
+++ quiz_script.js
@@ -3,9 +3,17 @@
 //const question1 = document.getElementById('question1')
 //question1.addEventListener.preventDefault()
 
-//document.getElementById("submit1").addEventListener.preventDefault()
+document.getElementById("submit1").addEventListener("click", question1)
 
-function question1 (form) {
-    const answer = document.getElementById(question_1).value
-    console.log(answer)
+function question1() {
+    event.preventDefault()
+    var real_answer = document.getElementById("html")
+    //var c2 = document.getElementById("ruby")
+    //c2.checked = true
+
+    if (real_answer.checked == true) {
+        console.log("correct")
+    }   else    {
+        console.log("incorrect")
+    }
 }
