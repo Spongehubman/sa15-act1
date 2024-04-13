958e17c827d7969f9b6c42b13039b1b740cdfff9
Nicholas Folis <heffawoozle77@gmail.com>
2024-04-12 23:32:31 -0500

    I further modified 'quiz.html' by modifying the first three 'question spaces', and I have added three more questions. Further additions of code were added to 'quiz_script.js'.

--- quiz_script.js
+++ quiz_script.js
@@ -1,1 +1,11 @@
-const button = getById
+
+
+//const question1 = document.getElementById('question1')
+//question1.addEventListener.preventDefault()
+
+//document.getElementById("submit1").addEventListener.preventDefault()
+
+function question1 (form) {
+    const answer = document.getElementById(question_1).value
+    console.log(answer)
+}
